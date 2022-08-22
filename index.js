ca34a132db53e0be2b034b2deca256ef190a633d
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-22 21:19:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    const o = document.getElementById(`text`)
+    o.innerHTML = ``;
+    o.appendChild(document.createTextNode("This is really cool!"))
+    // set innerHTML to an empty string.
+
+    //Then we call document.createTextNode to create a new text node with the text in the argument.
+
+    //And then we call appendChild to insert the text node to the div.
+})
